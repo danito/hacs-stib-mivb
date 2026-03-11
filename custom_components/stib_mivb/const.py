a78e63df704a90014c7e09cadb97ac6743d4c66b
Daniel nix <daniel.nix@gmail.com>
2026-03-11 19:33:46 +0100

    Add files via upload

---
+++ custom_components/stib_mivb/const.py
@@ -0,0 +1,32 @@
+"""Constants for the STIB/MIVB integration."""
+
+DOMAIN = "stib_mivb"
+DEFAULT_SCAN_INTERVAL = 30  # seconds
+
+CONF_LANGUAGE = "language"
+CONF_LINE_ID = "line_id"
+CONF_STOP_IDS = "stop_ids"
+CONF_STOPS = "stops"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+LANGUAGE_FRENCH = "fr"
+LANGUAGE_DUTCH = "nl"
+LANGUAGE_OPTIONS = {
+    "French": LANGUAGE_FRENCH,
+    "Nederlands": LANGUAGE_DUTCH,
+}
+
+API_BASE = "https://api-management-discovery-production.azure-api.net/api/datasets/stibmivb"
+API_STOPS_BY_LINE = f"{API_BASE}/static/stopsByLine"
+API_STOP_DETAILS = f"{API_BASE}/static/StopDetails"
+API_WAITING_TIMES = f"{API_BASE}/rt/WaitingTimes"
+
+ATTR_NEXT_PASSAGE = "next_passage"
+ATTR_LATITUDE = "latitude"
+ATTR_LONGITUDE = "longitude"
+ATTR_STOP_NAME_FR = "stop_name_fr"
+ATTR_STOP_NAME_NL = "stop_name_nl"
+ATTR_DIRECTION = "direction"
+ATTR_DESTINATION = "destination"
+ATTR_LINE_ID = "line_id"
+ATTR_STOP_ID = "stop_id"
